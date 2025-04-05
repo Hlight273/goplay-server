a0018d1c66afd9413fba3224c7c44223c7ec5719
hlight <2732149714@qq.com>
2025-04-05 16:02:23 +0800

    修改部署脚本 未先清除已有进程问题

--- compress/runGoPlayOnServer - 副本.bat
+++ compress/runGoPlayOnServer - 副本.bat
@@ -1,1 +1,2 @@
-ssh root@IPPPPPPPP "cd /home/upload/golplay-release/jar && nohup java -jar goplay-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &"
+chcp 65001
+ssh root@IPPPPPPPPP "cd /home/upload/golplay-release/jar && lsof -t -i:8081 | xargs -r kill -9 && nohup java -jar goplay-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &"
